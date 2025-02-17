129e63962819c4ec1b131afeba5a5de83ba78e80
cojovi <31863818+cojovi@users.noreply.github.com>
2025-02-17 09:10:59 -0600

    updated contacts

--- src/data.ts
+++ src/data.ts
@@ -112,6 +112,646 @@ export const employees: Employee[] = [
     phone: '214-893-3493',
     imageUrl: '/favicon.png',
     position: 'Project Manager'
+  },
+  {
+    id: 'abel-salazar',
+    name: 'Abel Salazar',
+    email: 'abels@cmacroofing.com',
+    phone: '214-533-3078',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'alex-miles',
+    name: 'Alex Miles',
+    email: 'alexm@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'alfredo-sandoval',
+    name: 'Alfredo Sandoval',
+    email: 'alfredos@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'allan-aviles',
+    name: 'Allan Aviles',
+    email: 'allan@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'alondra-flores',
+    name: 'Alondra Flores',
+    email: 'alondraf@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'amy-havenor',
+    name: 'Amy Havenor',
+    email: 'amyh@cmacroofing.com',
+    phone: '214-734-2717',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'andrea-lund',
+    name: 'Andrea Lund',
+    email: 'andreal@cmacroofing.com',
+    phone: '+16822611092',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'anna-skwierinski',
+    name: 'Anna Skwierinski',
+    email: 'annas@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'ashlee-eaton',
+    name: 'Ashlee Eaton',
+    email: 'ashlee@cmacroofing.com',
+    phone: '2146364118',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'avery-west',
+    name: 'Avery West',
+    email: 'averyw@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'brian-luciano',
+    name: 'Brian Luciano',
+    email: 'brianl@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'brock-johnston',
+    name: 'Brock Johnston',
+    email: 'brock@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'chasity-jones',
+    name: 'Chasity Jones',
+    email: 'chasityj@cmacroofing.com',
+    phone: '940-393-9157',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'chris-reynolds',
+    name: 'Chris Reynolds',
+    email: 'chrisr@cmacroofing.com',
+    phone: '+12817708926',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'chris-harrison',
+    name: 'Chris Harrison',
+    email: 'chrish@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'chris-singleton',
+    name: 'Chris Singleton',
+    email: 'chriss@cmacroofing.com',
+    phone: '+12148768630',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'christian-sommer',
+    name: 'Christian Sommer',
+    email: 'csommer@cmacroofing.com',
+    phone: '+12818508678',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'christian-viveiros',
+    name: 'Christian Viveiros',
+    email: 'christian@cmacroofing.com',
+    phone: '+18174717854',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'cody-viveiros',
+    name: 'Cody Viveiros',
+    email: 'codyv@cmacroofing.com',
+    phone: '+18177512041',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'daniel-lara',
+    name: 'Daniel Lara',
+    email: 'daniell@cmacroofing.com',
+    phone: '+18179406899',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'daniel-arreola',
+    name: 'Daniel Arreola',
+    email: 'daniela@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'david-havenor',
+    name: 'David Havenor',
+    email: 'davidh@cmacroofing.com',
+    phone: '+12148834670',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'dulce-munoz',
+    name: 'Dulce Munoz',
+    email: 'candym@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'edwin-escobar',
+    name: 'Edwin Escobar',
+    email: 'edwine@cmacroofing.com',
+    phone: '682-521-7722',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'eric-francis',
+    name: 'Eric Francis',
+    email: 'ericf@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'faith-mahan',
+    name: 'Faith Mahan',
+    email: 'faith@cmacroofing.com',
+    phone: '+18179629018',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'hugo-sandia',
+    name: 'Hugo Sandia',
+    email: 'hugos@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'hunter-powers',
+    name: 'Hunter Powers',
+    email: 'hunterp@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'ivan-guadiana',
+    name: 'Ivan Guadiana',
+    email: 'ivang@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'ivis-aviles',
+    name: 'Ivis Aviles',
+    email: 'ivis@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'jace-hobbs',
+    name: 'Jace Hobbs',
+    email: 'jace@cmacroofing.com',
+    phone: '+16825517020',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'jared-hobbs',
+    name: 'Jared Hobbs',
+    email: 'jared@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'jason-carson',
+    name: 'Jason Carson',
+    email: 'jasonc@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'javier-sosa',
+    name: 'Javier Sosa',
+    email: 'javiers@cmacroofing.com',
+    phone: '+14698068207',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'jeninne-glass',
+    name: 'Jeninne Glass',
+    email: 'jeninneg@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'jeremy-smith',
+    name: 'Jeremy Smith',
+    email: 'jeremys@cmacroofing.com',
+    phone: '817-614-2199',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'john-rogers',
+    name: 'John Rogers',
+    email: 'johnr@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'johnny-thacker',
+    name: 'Johnny Thacker',
+    email: 'johnnyt@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'jonathan-haigwood',
+    name: 'Jonathan Haigwood',
+    email: 'jonathanh@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'jonni-torres',
+    name: 'Jonni Torres',
+    email: 'jonni@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'jordan-west',
+    name: 'Jordan West',
+    email: 'jordanw@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'jorge-marquez',
+    name: 'Jorge Marquez',
+    email: 'jorgem@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'joseph-bandy',
+    name: 'Joseph Bandy',
+    email: 'josephb@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'jr-aviles',
+    name: 'JR Aviles',
+    email: 'jr@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'juan-mata',
+    name: 'Juan Mata',
+    email: 'juanm@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'larry-cremean',
+    name: 'Larry Cremean',
+    email: 'larryc@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'lazaro-castillo',
+    name: 'Lazaro Castillo',
+    email: 'lazaroc@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'leo-don',
+    name: 'Leo Don',
+    email: 'leo@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'leticia-escamilla',
+    name: 'Leticia Escamilla',
+    email: 'leticiae@cmacroofing.com',
+    phone: '+18176894391',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'lily-castillo',
+    name: 'Lily Castillo',
+    email: 'lilyc@cmacroofing.com',
+    phone: '972-832-3677',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'luis-gutierrez',
+    name: 'Luis Gutierrez',
+    email: 'luisg@cmacroofing.com',
+    phone: '+19452466775',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'mac-brink',
+    name: 'Mac Brink',
+    email: 'macb@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'mario-dixon',
+    name: 'Mario Dixon',
+    email: 'mariod@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'martin-m',
+    name: 'Martin M',
+    email: 'martinm@cmacroofing.com',
+    phone: '+18326907898',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'martin-l',
+    name: 'Martin L',
+    email: 'martinl@cmacroofing.com',
+    phone: '+14696262412',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'marvin-bonilla',
+    name: 'Marvin Bonilla',
+    email: 'marvinb@cmacroofing.com',
+    phone: '817-705-5065',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'michael-bennett',
+    name: 'Michael Bennett',
+    email: 'michaelb@cmacroofing.com',
+    phone: '+18178291392',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'micheal-sirenel',
+    name: 'Micheal Sirenel',
+    email: 'micheals@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'miguel-sandia',
+    name: 'Miguel Sandia',
+    email: 'miguels@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'mike-porter',
+    name: 'Mike Porter',
+    email: 'mikep@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'monica-lopez',
+    name: 'Monica Lopez',
+    email: 'monical@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'omar-guadiana',
+    name: 'Omar Guadiana',
+    email: 'omarg@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'oscar-salazar',
+    name: 'Oscar Salazar',
+    email: 'oscar@cmacroofing.com',
+    phone: '512-965-7256',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'pedro-marquez',
+    name: 'Pedro Marquez',
+    email: 'pedrom@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'renee-cremean',
+    name: 'Renee Cremean',
+    email: 'renee@cmacroofing.com',
+    phone: '817-296-3139',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'richard-rogers',
+    name: 'Richard Rogers',
+    email: 'richr@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'rob-davis',
+    name: 'Rob Davis',
+    email: 'rob@cmacroofing.com',
+    phone: '+18178884757',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'robin-garner',
+    name: 'Robin Garner',
+    email: 'robin@cmacroofing.com',
+    phone: '+18177974979',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'shane-gresham',
+    name: 'Shane Gresham',
+    email: 'shane@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'silvano-rojo',
+    name: 'Silvano Rojo',
+    email: 'silvanor@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'stephanie-smith',
+    name: 'Stephanie Smith',
+    email: 'stephanies@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'stephanie-ramos',
+    name: 'Stephanie Ramos',
+    email: 'stephanier@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'tavo-at-cmac-roofing',
+    name: 'Tavo at CMAC Roofing',
+    email: 'tavo@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'timothy-toshiba',
+    name: 'Timothy Toshiba',
+    email: 'timothytoshiba@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'veronica-paredes',
+    name: 'Veronica Paredes',
+    email: 'veronicag@cmacroofing.com',
+    phone: '+16823958721',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'victor-at-cmac-roofing',
+    name: 'Victor at CMAC Roofing',
+    email: 'victor@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'victor-jr-garcia',
+    name: 'Victor Jr Garcia',
+    email: 'victorjr@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'virginia-martinez',
+    name: 'Virginia Martinez',
+    email: 'virginiam@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'vivian-torres',
+    name: 'Vivian Torres',
+    email: 'vivian@cmacroofing.com',
+    phone: '+18177052676',
+    imageUrl: '/favicon.png',
+    position: 'emply'
+  },
+  {
+    id: 'wes-shierry',
+    name: 'Wes Shierry',
+    email: 'wes@cmacroofing.com',
+    phone: '',
+    imageUrl: '/favicon.png',
+    position: 'emply'
   }
 ];
 
